3083f3b92f3d87719d14370ce8ea27945253ec6c
lisuting <lisuting@founq.com>
2019-06-03 10:04:00 +0800

    http-proxy

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const proxy = require('http-proxy-middleware')
 const compression = require('compression')
 const UserRouter = require('./router/user.router')
 const IngredientRouter = require('./router/ingredient.router')
@@ -13,17 +14,18 @@ app.use(bodyParser.json({limit:'50mb'}))
 app.use(bodyParser.urlencoded({limit:'50mb',extended:true,parameterLimit:50000}))
 app.use(compression())
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-  if (req.method == 'OPTIONS') {
-    res.send(200); /*让options请求快速返回*/
-  } else {
-    next();
-  }
-})
+app.use('/province/**',proxy({target:'http://cpnp.nmpa.gov.cn',changeOrigin:true}))
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//
+//   if (req.method == 'OPTIONS') {
+//     res.send(200); /*让options请求快速返回*/
+//   } else {
+//     next();
+//   }
+// })
 
 
 //router manager
@@ -43,7 +45,8 @@ app.get('*',function(req,res){
 
 //connect to mongodb
 mongoose.set('useCreateIndex', true)
-mongoose.connect('mongodb://suky:00000000@47.100.171.180:27017/admin', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/admin', {useNewUrlParser: true})
+// mongoose.connect('mongodb://suky:00000000@47.100.171.180:27017/admin', {useNewUrlParser: true})
 mongoose.connection.on('connected',() => console.log('mongodb is now connected'))
 
 const PORT =process.env.PORT || 3001
